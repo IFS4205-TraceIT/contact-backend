1026b069a679e278bf82c60f073f32fb63313300
Styz <sky.t4y@gmail.com>
2022-10-08 15:58:34 +0800

    Update to use datetime

--- buildings/serializers.py
+++ buildings/serializers.py
@@ -1,9 +1,9 @@
 from .models import Buildings,Buildingaccess
 from rest_framework import serializers 
-from datetime import date
+from datetime import datetime
 
 class BuildingRegisterSerializer(serializers.ModelSerializer):
-    access_timestamp = serializers.DateTimeField(default=date.today())
+    access_timestamp = serializers.DateTimeField(default=datetime.now())
     class Meta:
         model = Buildingaccess
         fields = '__all__'
