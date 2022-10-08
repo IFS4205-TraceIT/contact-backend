47f62096f0b2483a4668b62e9560d38fb3bb86a8
Styz <sky.t4y@gmail.com>
2022-10-08 16:06:39 +0800

    Remove default field

--- buildings/serializers.py
+++ buildings/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from datetime import datetime
 
 class BuildingRegisterSerializer(serializers.ModelSerializer):
-    access_timestamp = serializers.DateTimeField(default=datetime.now())
     class Meta:
         model = Buildingaccess
         fields = '__all__'
