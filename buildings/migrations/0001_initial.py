bf15a7fe5d21b7f14f102011c1cdcc07f650434a
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2022-10-10 23:09:40 +0800

    Add DB and CA settings, and change building ID

--- buildings/migrations/0001_initial.py
+++ buildings/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -16,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Buildings',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('name', models.TextField()),
                 ('location', models.IntegerField()),
             ],
