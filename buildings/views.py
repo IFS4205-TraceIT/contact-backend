daf5a057bd540421c6fb805a3ef1b416a42825f9
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2022-10-23 15:05:29 +0800

    Add logging (#14)
    * Add logging
    
    * Add logging config to settings.py
    
    * Fix logger name
    
    * Change log output to be dependent on DEBUG
    
    * Add JSON and Loki logging packages
    
    * Add blank auth if in debug mode
    
    * Set disable_existing_loggers and propagate dependant on DEBUG
    
    Co-authored-by: arraysius <16810847+arraysius@users.noreply.github.com>

--- buildings/views.py
+++ buildings/views.py
@@ -12,6 +12,9 @@ import qrcode
 import io
 import base64
 
+import logging
+logger = logging.getLogger('loki')
+
 # Create your views here.
 
 class GenerateQRCodeView(APIView):
@@ -30,6 +33,7 @@ class GenerateQRCodeView(APIView):
         img_save = img.save(tmp)
         png_qr = tmp.getvalue()
         b64_img = base64.b64encode(png_qr)
+        logger.info('Generated Building QR code.', extra={'action': 'generate_building_qr_code', 'request': request, 'building_id': building.id})
         return Response(data={"qrcode": b64_img}, status=status.HTTP_200_OK)
 
 class BuildingAccessRegister (CreateAPIView):
@@ -37,18 +41,20 @@ class BuildingAccessRegister (CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            building = Buildings.objects.get(id=request.data['building'])
-        except:
-            raise ValidationError(detail="Building does not exist")
-        try:
             user = Users.objects.get(id=request.user.id)
         except Users.DoesNotExist:
             raise ValidationError(detail="User does not exist")
+        try:
+            building = Buildings.objects.get(id=request.data['building'])
+        except:
+            logger.warn('Building does not exist.', extra={'action': 'building_access', 'request': request, 'user_id': user.id})
+            raise ValidationError(detail="Building does not exist")
             
         infection = user.infectionhistory_set.filter( 
             recorded_timestamp__range=(datetime.combine(date.today(), datetime.min.time())-timedelta(days=15), datetime.today().replace(hour=23, minute=59, second=59, microsecond=999999))
             )
         if infection.exists():
+            logger.info('User has recent infection history.', extra={'action': 'building_access', 'request': request, 'user_id': user.id})
             return Response(data={'building_name': building.name, 'infected':True}, status=status.HTTP_200_OK)
 
         request.data['user'] = request.user.id
@@ -56,5 +62,7 @@ class BuildingAccessRegister (CreateAPIView):
         buildingaccess = BuildingRegisterSerializer(data=request.data)
         buildingaccess.is_valid(raise_exception=True)
         buildingaccess.save()
+
+        logger.info('User accessed building.', extra={'action': 'building_access', 'request': request, 'user_id': request.user.id, 'building_id': building.id})
 
         return Response(data={'building_name': building.name, 'infected': False}, status=status.HTTP_201_CREATED)
