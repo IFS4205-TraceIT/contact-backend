029ec20c92f2a01de441fa1fbe7b7a70082ea683
Styz <sky.t4y@gmail.com>
2022-10-25 01:37:40 +0800

    Remove QRCode backend

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
 from .views import (
-    GenerateQRCodeView,
     BuildingAccessRegister
 )
 
 app_name = 'infections'
 
 urlpatterns = [
-    path('genqrcode/<name>', GenerateQRCodeView.as_view()),
     path('register', BuildingAccessRegister.as_view()),
 ]
