2c370547f979b9183378e4ff8ef299cdc8aa6756
Styz <sky.t4y@gmail.com>
2022-10-27 15:20:04 +0800

    Include rest_framework in installed apps

--- contact_backend/settings.py
+++ contact_backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'accounts.apps.AccountsConfig',
     'contacts.apps.ContactsConfig',
